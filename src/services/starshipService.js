acb01dc72afe0cce5fe7f97686a14b29521ffa2f
MShubar <pokeeeeepow@gmail.com>
2024-12-19 14:04:18 +0300

    Why grade 0?

--- src/services/starshipService.js
+++ src/services/starshipService.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://swapi.dev/api/starships/'
+const BASE_URL = "https://swapi.py4e.com/api/starships"
 
 export async function getStarships() {
   const response = await fetch(BASE_URL)
-  if (!response.ok) throw new Error('Failed to fetch starships')
+  if (!response.ok) throw new Error("Failed to fetch starships")
   return await response.json()
 }
